aac00e561416dfcd35a61899edd22f490233da28
Tony-De-Donato <113009524+Tony-De-Donato@users.noreply.github.com>
2022-09-30 21:11:51 +0200

    Add files via upload

---
+++ tache_3.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	for compteur := '0'; compteur <= '9'; compteur++ {
+		z01.PrintRune(compteur)
+	}
+}
---
+++ tache_6.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				for l := j + 1; l <= '9'; l++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+					z01.PrintRune(',')
+
+				}
+			}
+		}
+
+	}
+
+}
+
+func main() {
+	PrintComb2()
+}
